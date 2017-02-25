af02aaecba1c4c1ccb0eeca9954a1393648a0610
Brennan Boyle <brennanboyle@gmail.com>
2017-02-25 03:11:16 -0600

    add colors and styling to list items

--- app/src/main/java/com/example/brennan/lister/TaskAdapter.java
+++ app/src/main/java/com/example/brennan/lister/TaskAdapter.java
@@ -1,6 +1,7 @@
 package com.example.brennan.lister;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -32,6 +33,21 @@ public class TaskAdapter extends ArrayAdapter<Task> {
 
         // Populate the data into the template view using the data object
         taskTitle.setText(task.getTitle());
+        // set task color by its priority
+        switch(task.getPriority()){
+            case "Low Priority":
+                // set bg color to light green
+                convertView.setBackgroundColor(Color.rgb(173, 237, 173));
+                break;
+            case "High Priority":
+                // set bg color to gentle red
+                convertView.setBackgroundColor(Color.rgb(239, 110, 110));
+                break;
+            default:
+                // normal priority, set bg color to light yellow
+                convertView.setBackgroundColor(Color.rgb(253, 253, 150));
+                break;
+        }
         // Return the completed view to render on screen
         return convertView;
     }
